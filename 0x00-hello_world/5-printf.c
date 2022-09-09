fc3e45c7d33a225e362b6bc68fcd2cc150cc3f26
WidsonMauya <widsonmauya@gmail.com>
2022-09-08 19:30:39 -0700

    my commit to tas 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ *main-print the string in the put function
+ *
+ *Description:using the main function
+ *this program prints "with proper grammar, but the outcome is a piece of art"
+ *Return:0
+ */
 int main(void)
 
 {
