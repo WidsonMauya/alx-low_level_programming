199c476d5a985b8d89b23896145ce4a31a4b2143
WidsonMauya <widsonmauya@gmail.com>
2022-09-08 19:07:13 -0700

    my commit on task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 
 /**
 * Description:use the main function
-* main - print the string in the put function
+* main-print the string in the put function
 * Return: Always 0 (Success)
+* thi program prints"Programming is like building a multilingual puzzle"
 */
 
 int main(void)
