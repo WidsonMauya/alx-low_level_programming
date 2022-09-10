66f5af7a1f32c3785d59cb13cda396a508038ec5
WidsonMauya <widsonmauya@gmail.com>
2022-09-10 01:32:06 -0700

    Write a C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main-entry point
- *
- * Description: prints and that peace of art is useful.."without puts
- * Return: 1 if successful
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-char *s = " and that piece of art is useful\"- Dora korpar, 2015-10-19\n";
-long i = 59;
-long fd 1;
-long syscall = 1;
-long ret = 0;
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 
-_asm_("syscall"
-		: "=a" (ret)
-		: "a" (syscall),
-		"D" (fd),
-		"S" (s),
-		"d" (1));
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 
 return (1);
 
