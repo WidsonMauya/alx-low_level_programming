d9d8ad7a5e8a8cbf31a9309acb264dfef8910cc0
WidsonMauya <widsonmauya@gmail.com>
2022-09-16 08:28:42 -0700

    a function that prints the numbers, from 0 to 9, followed by a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print number  bweteen 0 and 9 exclude 2 & 4
+ */
+
+void print_numbers(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
